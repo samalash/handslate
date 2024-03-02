3ed9f1dd93f7c137b95a32828fe01862c06d3026
Drew Crispino <andrewvcrispino@gmail.com>
2024-03-02 14:33:49 -0500

    Created html templates

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, Blueprint
 
 app = Flask(__name__)
+bp = Blueprint("pages", __name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template("pages/home.html")
+
+@app.route("/about")
+def about():
+    return render_template("pages/about.html")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
